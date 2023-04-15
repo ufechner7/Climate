172db69ce4ade00c897cd121892f076c1c749da5
Uwe Fechner <uwe.fechner.msc@gmail.com>
2023-04-15 22:10:22 +0200

    Some improvements

--- src/simulate.jl
+++ src/simulate.jl
@@ -8,7 +8,7 @@ const CO2_wind_land  =   5.6  # g per kWh
 WIND_ENERGY_PER_TURBINE = 440 # GWh in 20 years
 CONSUMPTION_PER_YEAR =   6440 # GWh per year
 LIFE_TIME = 20                # lifetime of a turbine in years
-YEARS = 20                    # simulation time in years
+YEARS = 10                    # simulation time in years
 WINDTURBINE_CO2 = WIND_ENERGY_PER_TURBINE * 1e6 * CO2_wind_land / 1e6
 
 mutable struct State
@@ -39,7 +39,7 @@ function calc_co2(s::State, energy)
     s.gas_co2_tot += s.gas_co2
 end
 
-function calc_scenario(turbines_per_year)
+function calc_scenario(turbines_per_year, prn=false)
     s = State(0, 0.0, 0.0, 0.0, 0.0)
     Y = zeros(YEARS)
     X = 1:YEARS
@@ -47,13 +47,13 @@ function calc_scenario(turbines_per_year)
         calc_co2(s, CONSUMPTION_PER_YEAR*1e6)
         build_turbines(s, turbines_per_year)
         co2 = s.wind_co2 + s.gas_co2
-        Y[i] = co2
+        Y[i] = co2/1e6
         println(i+2020, " CO2 emmisions: $co2 tons")
     end
     X, Y
 end
 
-X, Y = calc_scenario(16)
-plot(X, Y, label="yearly CO2 emmissions [t]")
+X, Y = calc_scenario(32, true)
+plot(X, Y, label="yearly CO2 emmissions [Mt]")
 X1, Y1 = calc_scenario(0)
-plot!(X, Y1, label="yearly emmisions without turbines [t]")
+plot!(X, Y1, label="yearly emmisions without turbines [Mt]")
